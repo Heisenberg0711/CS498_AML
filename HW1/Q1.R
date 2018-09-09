d70de4890d47bd42db2c1e53d30e411d8760fcdf
Heisenberg0711 <heisenberg0711@gmail.com>
2018-09-08 21:46:04 -0500

    finished 1a 1b

--- HW1/Q1.R
+++ HW1/Q1.R
@@ -1,5 +1,6 @@
 #Part 1A
 library(caret)
+set.seed(406)
 Diabete_data <- read.csv(file = "pima-indians-diabetes.csv", header = TRUE, sep = ",")
 
 #Split the data 10 times and fill in the accuracy vector
@@ -76,28 +77,27 @@ avg_accuracy1 <- mean(accuracy1)
 
 
 #Part 1B
-library(caret)
+#Split the data 10 times and fill in the accuracy vector
+accuracy2 = double(length = 10)
+itr = 1
+while (itr < 11) {
+#Re-Read the data for every iteration to ensure not all 0 are marked as NA
 Diabete_data <- read.csv(file = "pima-indians-diabetes.csv", header = TRUE, sep = ",")
+train_Index <- createDataPartition(Diabete_data$X1, p = 0.2, list = FALSE, times = 1)
+
 num_attr = length(Diabete_data)
 
 #Mark the zeros in attribute 3,4,6,8 as NA
 for (attr in 1:num_attr) {
   if (attr == 3 | attr == 4 | attr == 6 | attr == 8) {
-     curr_attr = Diabete_data[[attr]]
-     for (entry in 1:length(curr_attr)) {
-        if (curr_attr[entry] == 0) {
-          Diabete_data[[attr]][entry] <- NA
-        }
-     }  
+    curr_attr = Diabete_data[[attr]]
+    for (entry in train_Index) {
+      if (curr_attr[entry] == 0) {
+        Diabete_data[[attr]][entry] <- NA
+      }
+    }  
   }
 }
-
-#Split the data 10 times and fill in the accuracy vector
-accuracy2 = double(length = 10)
-itr = 1
-while (itr < 11) {
-
-train_Index <- createDataPartition(Diabete_data$X1, p = 0.2, list = FALSE, times = 1)
 
 diabete_testY <- Diabete_data$X1[-train_Index]
 diabete_trainY <- Diabete_data$X1[train_Index]
@@ -137,11 +137,8 @@ result = integer(length(diabete_testY))
       prob1 = double(length = N)
       for (property in 1:N) {
         curr_prop = (Diabete_data[[property]][-train_Index])[entry]
-        #To check if the current entry is NA, if not, proceed. 
-        if (!is.na(curr_prop)) {
         prob0[property] <- dnorm(curr_prop, mean = X0_Means[property], sd = X0_Stdev[property], log = TRUE)
         prob1[property] <- dnorm(curr_prop, mean = X1_Means[property], sd = X1_Stdev[property], log = TRUE)
-        }  
       }
       if (sum(prob0) + log(p0y) >= sum(prob1) + log(p1y)) {
         result[entry] <- 0 
@@ -162,3 +159,5 @@ result = integer(length(diabete_testY))
 }
 
 avg_accuracy2 = mean(accuracy2)
+
+

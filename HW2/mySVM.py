564590ba22c2442aefbb76363d15c277f40b0db2
Haotian_Qiu <heisenberg0711@gmail.com>
2018-09-15 18:51:11 -0500

    almost there

--- HW2/mySVM.py
+++ HW2/mySVM.py
@@ -9,12 +9,12 @@ from sklearn.model_selection import ShuffleSplit
 #This function is used to convert string to values
 def label_income(row):
     if row[14] == " <=50K":
-        return 0
+        return -1
     if row[14] == " >50K":
         return 1
 
 #Detect NA values and mark them as NA
-df = pd.read_csv('train.data', header = None, na_values=" ?")
+df = pd.read_csv('train.data', header = None)
 df = df.dropna(axis=0,how='any')
 labels = df.apply(lambda row: label_income (row),axis=1).values
 
@@ -35,33 +35,61 @@ train_features, test_features, train_labels, test_labels = train_test_split(feat
 lamda_vals = np.array([1e-3,1e-2,1e-1,1])
 
 #Define constants for calculating stepsize = a / (epoch + b)
-a = 6
-b = 2
+m = 6
+n = 2
 
 #Number of steps per season
 N_steps = 300
 
 #This function calculates gradient and updates the values of a and b
-#returns an numpy array of a and b
+#returns a tuple of a vector and b value
 def gradient(a, b, X, Y,lam):
-    if Y*(a.T*X + b) < 1:
-        return np.array([lam*a - Y*X, -Y])
-    else:
-        return np.array([lam*a, 0])
+    mat = Y * (np.dot(X,a) + b)
+    idx1 = np.where(mat >= 1)
+    idx0 = np.where(mat < 1)
+
+    posA = len(idx1[1])*lam*a
+    negA = len(idx0[0])*lam*a - np.sum(Y[idx0]*X[idx0], axis = 0)
+    negB = -np.sum(Y[idx0])
+    return (posA+negA, negB)
 
 
-msk = np.array(range(0,len(train_labels)))
-np.random.shuffle(msk)
+#This function will give the results of the model
+def get_accu(a, b, val_features, val_labels):
+    prediction = val_features.dot(a)
+    result = np.where(prediction > 0, 1, -1).reshape(50,)
+    print(result.shape)
+    print(val_labels.shape)
+    print(np.sum(result == val_labels))
+    accuracy = np.sum(result==val_labels) / val_labels.shape[0]
+    return accuracy
 
-held_features = np.array(train_features[msk[-50:]])
-held_labels = np.array(train_labels[msk[-50:]])
-train_bound = len(msk)-50
-batch_size = ma.ceil(train_bound / N_steps)
 
-for step in range(N_steps):
-    X = train_features[msk[step * batch_size: (step + 1) * batch_size]]
-    print((step+1) * batch_size)
-    print(X.shape)
 
-print("the length of the mask is")
-print(len(msk))
+lam = 1e-2
+
+for epoch in arange(1,51):
+
+    ida = m / (epoch + n)
+    a = np.random.random([6, 1])
+    b = np.random.random()
+
+    for step in range(300):
+        #Create a mask to choose batches for each epoch
+        msk = np.array(range(0,len(train_labels)))
+        np.random.shuffle(msk)
+
+        held_features = np.array(train_features[msk[-50:]])
+        held_labels = np.array(train_labels[msk[-50:]])
+        Index_array = np.array_split(msk[:-50], N_steps)
+
+        #for step in range(1):
+
+
+
+        batch_size = len(Index_array[0])
+        X = train_features[Index_array[0]]
+        Y = train_labels[Index_array[0]].reshape(batch_size, 1)
+        gradA, gradB = gradient(a,b,X,Y,0.1)
+        a = a - ida * (1/batch_size)*gradA
+        b = b - ida * (1/batch_size)*gradB

414ea17c1511a5d7ac329160fc2674288709cd18
Haotian_Qiu <heisenberg0711@gmail.com>
2018-09-15 23:37:17 -0500

    completed

--- HW2/mySVM.py
+++ HW2/mySVM.py
@@ -13,10 +13,14 @@ def label_income(row):
     if row[14] == " >50K":
         return 1
 
+
+def parse(row):
+    return "'" + row + "'"
+
 #Detect NA values and mark them as NA
 df = pd.read_csv('train.data', header = None)
 df = df.dropna(axis=0,how='any')
-labels = df.apply(lambda row: label_income (row),axis=1).values
+labels = df.apply(lambda row: label_income(row),axis=1).values
 
 #Exclude non-continuous values
 df = df.drop(columns = [1,3,5,6,7,8,9,13])
@@ -35,11 +39,12 @@ train_features, test_features, train_labels, test_labels = train_test_split(feat
 lamda_vals = np.array([1e-3,1e-2,1e-1,1])
 
 #Define constants for calculating stepsize = a / (epoch + b)
-m = 6
-n = 2
+m = 1
+n = 50
 
-#Number of steps per season
+#Number of steps per season and regularization constants
 N_steps = 300
+lam = np.array([1e-3, 0.002, 0.0025, 1e-2, 1e-1, 1])
 
 #This function calculates gradient and updates the values of a and b
 #returns a tuple of a vector and b value
@@ -55,26 +60,25 @@ def gradient(a, b, X, Y,lam):
 
 
 #This function will give the results of the model
-def get_accu(a, b, val_features, val_labels):
-    prediction = val_features.dot(a)
-    result = np.where(prediction > 0, 1, -1).reshape(50,)
-    print(result.shape)
-    print(val_labels.shape)
-    print(np.sum(result == val_labels))
+def get_accu(a, b, val_features, val_labels, size):
+    prediction = val_features.dot(a) + b
+    result = np.where(prediction > 0, 1, -1).reshape(size,)
     accuracy = np.sum(result==val_labels) / val_labels.shape[0]
     return accuracy
 
 
-
-lam = 1e-2
-
-for epoch in arange(1,51):
-
-    ida = m / (epoch + n)
+def SVM(train_features, train_labels, lam):
     a = np.random.random([6, 1])
     b = np.random.random()
 
-    for step in range(300):
+    #Empty accuracy array and magnitude array
+    accu_array = []
+    mag_array = []
+
+    for epoch in range(1,51):
+        #Calculate stepsize based on epoch
+        ida = m / (0.01 * epoch + n)
+
         #Create a mask to choose batches for each epoch
         msk = np.array(range(0,len(train_labels)))
         np.random.shuffle(msk)
@@ -83,13 +87,51 @@ for epoch in arange(1,51):
         held_labels = np.array(train_labels[msk[-50:]])
         Index_array = np.array_split(msk[:-50], N_steps)
 
-        #for step in range(1):
+        for step in range(300):
+            batch_size = len(Index_array[step])
+            X = train_features[Index_array[step]]
+            Y = train_labels[Index_array[step]].reshape(batch_size, 1)
+            gradA, gradB = gradient(a, b, X, Y, lam)
+            a = a - ida * (1/batch_size)*gradA
+            b = b - ida * (1/batch_size)*gradB
+
+            if (step % 30 == 0):
+                accu_array.append(get_accu(a, b, held_features, held_labels, 50))
+                mag_array.append(a.T.dot(a)[0,0])
+    return (a, b, accu_array, mag_array)
+
+
+#Call the SVM function and plot accuracy and magnitudes
+#for reg in lam:
+a, b, accu, mag = SVM(train_features, train_labels, lam[3])
+ans = get_accu(a, b, test_features, test_labels, len(test_labels))
 
 
 
-        batch_size = len(Index_array[0])
-        X = train_features[Index_array[0]]
-        Y = train_labels[Index_array[0]].reshape(batch_size, 1)
-        gradA, gradB = gradient(a,b,X,Y,0.1)
-        a = a - ida * (1/batch_size)*gradA
-        b = b - ida * (1/batch_size)*gradB
+#Use the given test data to validate the bodel
+df_val = pd.read_csv('test.data', header = None)
+df_val = df_val.dropna(axis=0, how='any')
+val_features = df_val.drop(columns = [1,3,5,6,7,8,9,13]).values
+val_features = val_features.astype(float)
+
+#Normalizing validation features
+for col in range(val_features.shape[1]):
+    item = val_features[:,col]
+    val_features[:,col] = (item - item.mean()) / item.std()
+
+val_pred = val_features.dot(a) + b
+val_result = np.where(val_pred > 0, 1, -1).reshape(len(val_pred),)
+val_result = list(val_result)
+
+for i in range(len(val_result)):
+    if val_result[i] == 1:
+        val_result[i] = ">50K"
+    else:
+        val_result[i] = "<=50K"
+
+id = np.arange(0,len(val_result))
+dict = {'Example': id, 'Label': val_result}
+df = pd.DataFrame(data = dict)
+df['Example'] = df['Example'].apply(str)
+df['Example'] = df['Example'].apply(parse)
+df.to_csv("hqiu9.csv", index = False)

09ea26f6f6d3dcd103504ebb891329da91295a25
Haotian_Qiu <heisenberg0711@gmail.com>
2018-09-29 22:52:18 -0500

    a

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 
@@ -10,7 +10,7 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-
+#Problem 7.7 (b)
 #This function calculates MSE
 from scipy.spatial.distance import sqeuclidean
 def comp_err(A, B):
@@ -27,25 +27,32 @@ d5 = unpickle("cifar-10-batches-py/data_batch_5")
 
 #Get the average image of all category of all batches and store them in dictionary.
 data_batch = [d1,d2,d3,d4,d5]
-img_avg = dict.fromkeys([0, 1, 2, 3, 4])
-img_original = dict.fromkeys([0, 1, 2, 3, 4])
-err_matrix = np.zeros([5,10])
+img_center = dict.fromkeys([0, 1, 2, 3])
+img_avg = dict.fromkeys([0, 1, 2, 3])
+img_original = dict.fromkeys([0, 1, 2, 3])
+err_matrix = np.zeros([4,10])
 
-for batch in range(5):
-    imgs = {}
+for batch in range(4):
+    imgs_c = {}
     imgs_mean = {}
+    img_org = {}
     for n in range(10):
         keys = np.asarray(data_batch[batch][b'labels'])
         labels = np.where(keys == n)
-        imgs[n] = data_batch[batch][b'data'][labels] - np.mean(data_batch[batch][b'data'][labels], axis=0)
+        img_org[n] = data_batch[batch][b'data'][labels]
+        imgs_c[n] = data_batch[batch][b'data'][labels] - np.mean(data_batch[batch][b'data'][labels], axis=0)
         imgs_mean[n] = np.mean(data_batch[batch][b'data'][labels], axis=0)
+    img_center[batch] = imgs_c
     img_avg[batch] = imgs_mean
-    img_original[batch] = imgs
+    img_original[batch] = img_org
 
 
-for batch in range(5):
-    for cat in range(10):
+for batch in range(4):
+    for n in range(10):
         pca = PCA(n_components = 20, copy=True, whiten=False, svd_solver='full', iterated_power='auto')
-        pc = pca.fit_transform(img_original[batch][cat])
-        reform = pca.inverse_transform(pc) + img_avg[batch][cat]
-        err_matrix[batch, n] = comp_err(reform, img_original[batch][cat])
+        pc = pca.fit_transform(img_center[batch][n])
+        reform = pca.inverse_transform(pc) + img_avg[batch][n]
+        err_matrix[batch, n] = comp_err(reform, img_original[batch][n])
+        print(err_matrix)
+
+plt.plot(range(0,10), err_matrix[0])

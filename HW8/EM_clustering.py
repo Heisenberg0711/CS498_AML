e0d5ff82fe1724e9a32f422c509fed921d595c45
heisenberg <heisenberg0711@gmail.com>
2018-11-05 22:01:04 -0600

    start

--- HW8/EM_clustering.py
+++ HW8/EM_clustering.py
@@ -1,1 +1,9 @@
 import numpy as np
+from scipy import misc
+from scipy.spatial import distance
+from scipy.cluster.vq import kmeans
+
+def CloseCluster(pixel, p, nclusters):
+    pixelArray = np.tile(pixel,(nclusters,1))
+    dist = distance.cdist(pixelArray, p, 'euclidean')
+    return np.argmin(dist)
